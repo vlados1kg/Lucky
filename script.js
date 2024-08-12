76d5f071fe5fb5a63b39bb5982e07b7e06b46a79
Vladyslav Hliuzitskyi <115615420+gluzik@users.noreply.github.com>
2024-08-13 01:50:28 +0200

    add

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let input = document.querySelector(".button");
 let output = document.querySelector(".main-title");
+let buttonText = document.querySelector(".button-text");
 let count = 1;
 let one = "1.51";
 let two = "3.28";
@@ -8,6 +9,7 @@ let three = "3.26";
 output.innerHTML = "5.73";
 
 input.addEventListener("click", () => {
+    loading ()
     if (count === 1) {
         count++;
         setTimeout(() => {output.innerHTML = one;}, 1000)
@@ -17,4 +19,10 @@ input.addEventListener("click", () => {
     } else if (count === 3) {
         setTimeout(() => {output.innerHTML = three;}, 1000)
     }
-}, 2000)
+}, 2000);
+
+function loading () {
+    buttonText.innerHTML = '';
+    buttonText.innerHTML = "<img class=\"loading\" alt=\"\" src=\"./img/loading.png\">";
+    setTimeout(() => {buttonText.innerHTML = "Check";}, 1000)
+}
